920af6c0e658a15cf05408c99ebc2c45e4bf8360
Kingsley Enoka <kuenoka@mix.wvu.edu>
2021-02-05 20:16:44 -0600

    Initial Commit

---
+++ AccessibilityProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessibilityProject
+//
+//  Created by Kingsley Enoka on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

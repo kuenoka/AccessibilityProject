37e0bdda43702cf0fc49b09afe512064cb7960b7
Kingsley Enoka <kuenoka@mix.wvu.edu>
2021-02-06 21:42:50 -0600

    first commit

---
+++ AccessibilityProject/MyCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyCell.swift
+//  AccessibilityProject
+//
+//  Created by Kingsley Enoka on 2/5/21.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
